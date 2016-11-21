adb2fba6f5e9e7c31d6be228a9c631b9afe8d473
Loris Witschard <loris.wit@gmail.com>
2016-11-21 17:11:06 +0100

    3.1 Affichages et points de vue

--- src/platform/game/Simulator.java
+++ src/platform/game/Simulator.java
@@ -1,49 +1,79 @@
 package platform.game;
 
 import platform.game.level.Level;
+
 import java.util.ArrayList;
 import java.util.List;
-import platform.util.Box;
 
-import platform.util.Input;
-import platform.util.Loader;
-import platform.util.Output;
-import platform.util.Vector;
-import platform.util.View;
+import platform.util.*;
 
 /**
  * Basic implementation of world, managing a complete collection of actors.
  */
-public class Simulator implements World {
-
+public class Simulator implements World
+{
     private Loader loader;
-  
+    
+    private Vector currentCenter;
+    private double currentRadius;
+    private Vector expectedCenter;
+    private double expectedRadius;
+    
     /**
-     * Create a new simulator.
-     * @param loader associated loader, not null
-     * @param args level arguments, not null
+     * Create a new simulator .
+     *
+     * @param loader associated loader , not null
      */
-	public Simulator(Loader loader, String[] args) {
-        if (loader == null)
+    public Simulator(Loader loader, String[] args)
+    {
+        if(loader == null)
+        {
             throw new NullPointerException();
+        }
         this.loader = loader;
-      
-	}
-	
+        currentCenter = Vector.ZERO;
+        expectedCenter = Vector.ZERO;
+        currentRadius = 10.0;
+        expectedRadius = 10.0;
+    }
+    
+    @Override
+    public void setView(Vector center, double radius)
+    {
+        if(center == null)
+            throw new NullPointerException();
+        if(radius <= 0.0)
+            throw new IllegalArgumentException("radius must be positive");
+        expectedCenter = center;
+        expectedRadius = radius;
+    }
+    
     /**
      * Simulate a single step of the simulation.
-     * @param input input object to use, not null
+     *
+     * @param input  input object to use, not null
      * @param output output object to use, not null
      */
-	public void update(Input input, Output output) {
+    public void update(Input input, Output output)
+    {
+        double factor = 0.001;
+        currentCenter = currentCenter.mul(1.0 - factor).add(expectedCenter.mul(factor));
+        currentRadius = currentRadius * (1.0 - factor) + expectedRadius * factor;
         
-      // TO BE COMPLETED
-	}
-
+        View view = new View(input, output);
+        view.setTarget(currentCenter, currentRadius);
+        
+        Sprite sprite = loader.getSprite("heart.full");
+        Box zone = new Box(new Vector(0.0, 0.0), 2, 2);
+        view.drawSprite(sprite, zone);
+        
+        if(view.getMouseButton(1).isPressed())
+            setView(view.getMouseLocation(), 10.0);
+    }
+    
     @Override
-    public Loader getLoader() {
+    public Loader getLoader()
+    {
         return loader;
     }
-
-    
 }
--- src/platform/game/World.java
+++ src/platform/game/World.java
@@ -7,9 +7,19 @@ import platform.util.Vector;
 /**
  * Represents an environment populated by actors.
  */
-public interface World {
-
-    /** @return associated loader, not null */
+public interface World
+{
+    
+    /**
+     * @return associated loader, not null
+     */
     public Loader getLoader();
-  
+    
+    /**
+     * Set viewport location and size.
+     *
+     * @param center viewport center , not null
+     * @param radius viewport radius , positive
+     */
+    public void setView(Vector center, double radius);
 }
