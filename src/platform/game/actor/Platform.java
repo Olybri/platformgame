baade76700b033b3d887d02f78db465528d46e8b
Loris Witschard <loris.wit@gmail.com>
2016-12-14 11:46:53 +0100

    Fix platform collision

--- src/platform/game/actor/Platform.java
+++ src/platform/game/actor/Platform.java
@@ -1,19 +1,26 @@
 package platform.game.actor;// Created by Loris Witschard on 11/29/2016.
 
 import platform.util.Box;
+import platform.util.Input;
+import platform.util.Output;
+import platform.util.Vector;
 
 /**
  * Block that is solid for actor falling from above.
  */
 public class Platform extends Block
 {
+    private Box fullbox;
+    
     /**
-     * @param box        bounding box of the platform
+     * @param box bounding box of the platform
      * @param spriteName name of the sprite to draw
      */
     public Platform(Box box, String spriteName)
     {
-        super(box, spriteName);
+        super(box.add(new Vector(0, 0.01)), spriteName);
+        
+        fullbox = box;
     }
     
     @Override
@@ -32,5 +39,11 @@ public class Platform extends Block
             && other.getPosition().getX() > getBox().getMin().getX() && other.getPosition().getX() < getBox().getMax().getX()
             && other.getBox().getMin().getY() > getBox().getMax().getY() - 0.2)
             other.interact(this);
+    }
+    
+    @Override
+    public void draw(Input input, Output output)
+    {
+        output.drawSprite(sprite, fullbox);
     }
 }
